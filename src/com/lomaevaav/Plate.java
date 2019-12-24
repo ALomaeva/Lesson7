d0e86920ec9bf04e66e950b3e3916e95d477bc26
Anastasiya Lomaeva <lomaevaav13@gmail.com>
2019-12-24 22:33:25 +0400

    Скопировала из методички

---
+++ src/com/lomaevaav/Plate.java
@@ -0,0 +1,14 @@
+package com.lomaevaav;
+
+public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
